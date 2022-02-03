ef5da054ea9bec3e627c92de498eafd2730a82a5
raymondcerneytech <cerne004@gmail.com>
2022-02-03 11:37:09 -0600

    began api formation

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,6 @@
+var apiKey = "a45105594dd7feb0a9436cca97d6a372";
+
+
 var searchButton = document.querySelector("#search-button");
 
 var cityForm = document.querySelector("#city-form");
@@ -7,5 +10,32 @@ var cityInput = document.querySelector("#city-input");
 
 var formSubmitHandler = function (event) {
 	event.preventDefault();
+
+	// get value from input element
+	var city = cityInput.value.trim();
+
+	if (city) {
+		getUserRepos(city);
+		nameInputEl.value = "";
+	} else {
+		alert("Please enter a GitHub username");
+	}
+
 	console.log(event);
 };
+
+function apiCall(city) {
+	var apiLink = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
+
+	let request = new XMLHttpRequest();
+	request.open("GET", apiLink);
+	request.send();
+	request.onload = () => {
+		console.log(request);
+		if (request.status === 200) {
+			console.log(JSON.parse(request.response));
+		} else {
+			console.log("error ${request.status} ${request.statusTest}");
+		}
+	}
+}
